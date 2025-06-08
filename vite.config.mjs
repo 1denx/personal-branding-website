19364b87a26e39f472f948aa8166a62b885a09d7
Eden <1denchen.work@gmail.com>
2025-06-08 18:07:30 +0800

    feat: 新增文章頁面

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
 export default defineConfig({
   base: "./",
   plugins: [tailwindcss()],
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        blog: resolve(__dirname, "src/blog.html"),
+      },
+    },
   },
 });
